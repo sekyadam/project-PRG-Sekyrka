72acbcba549d155057b3314635ebc18bfe817943
sekyrkaadam <sekyrkaadam@sssvt.cz>
2026-01-05 11:01:38 +0100

    save

---
+++ src/MovieAppProject/MovieAppProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieAppProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
